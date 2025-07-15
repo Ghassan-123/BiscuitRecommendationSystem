2cfc9299ba8b284e0daaa8a961a736e9d5a3e9ee
Ghassan-123 <ghassankk12342003@gmail.com>
2025-07-15 17:54:10 +0300

    great improvment (finished the basic output)

--- Main.py
+++ Main.py
@@ -15,32 +15,42 @@ class Main:
             "contaminated": {},
         }
 
-        recommendations = self.analyze_quality(quality_report)
+        #     recommendations = self.analyze_quality(quality_report)
 
-        print("Quality Control Recommendations:")
-        for i, rec in enumerate(recommendations, 1):
-            print(f"{i}. {rec}")
+        #     print("Quality Control Recommendations:")
+        #     for i, rec in enumerate(recommendations, 1):
+        #         print(f"{i}. {rec}")
 
-    def analyze_quality(self, quality_data):
+        # def analyze_quality(self, quality_data):
+        #     engine = RecommendationEngine()
+        #     engine.reset()
+
+        #     # Map must use lowercase keys matching quality_report
+        #     fact_classes = {
+        #         "cracked": Cracked,
+        #         "burned": Burned,
+        #         "under_cooked": UnderCooked,
+        #         "over_sized": OverSized,
+        #         "under_sized": UnderSized,
+        #         "contaminated": Contaminated,
+        #     }
+
+        #     for fact_name, params in quality_data.items():
+        #         if fact_name in fact_classes:
+        #             engine.declare(fact_classes[fact_name](**params))
+
+        #     engine.run()
+        #     return engine.recommendations
+
         engine = RecommendationEngine()
         engine.reset()
-
-        # Map must use lowercase keys matching quality_report
-        fact_classes = {
-            "cracked": Cracked,
-            "burned": Burned,
-            "under_cooked": UnderCooked,
-            "over_sized": OverSized,
-            "under_sized": UnderSized,
-            "contaminated": Contaminated,
-        }
-
-        for fact_name, params in quality_data.items():
-            if fact_name in fact_classes:
-                engine.declare(fact_classes[fact_name](**params))
-
         engine.run()
-        return engine.recommendations
+        for fact in engine.facts.values():
+            if fact.get(
+                "recommendation"
+            ):  # This checks for both existence and non-empty/None
+                print(dict(fact))  # Print all key-value pairs
+        print(engine.facts.values())
 
 
 if __name__ == "__main__":
--- RecommendationEngine.py
+++ RecommendationEngine.py
@@ -12,73 +12,86 @@ class RecommendationEngine(KnowledgeEngine):
         yield Fact(init=True)  # Simple initialization fact
         yield Question(
             id="total",
-            Type="input",
+            Type="input_int",
             valid=[],
             text="What is the batch size?",
         )
         yield Question(
             id="deffected",
-            Type="input",
+            Type="input_int",
             valid=[],
             text="how many biscuits are deffected?",
         )
         yield Question(
             id="cracked",
-            Type="input",
+            Type="input_int",
             valid=[],
             text="how many biscuits are cracked?",
         )
         yield Question(
             id="burned",
-            Type="input",
+            Type="input_int",
             valid=[],
             text="how many biscuits are burned?",
         )
         yield Question(
             id="under_cooked",
-            Type="input",
+            Type="input_int",
             valid=[],
             text="how many biscuits are under_cooked?",
         )
         yield Question(
             id="over_sized",
-            Type="input",
+            Type="input_int",
             valid=[],
             text="how many biscuits are over_sized?",
         )
         yield Question(
             id="under_sized",
-            Type="input",
+            Type="input_int",
             valid=[],
             text="how many biscuits are under_sized?",
         )
         yield Question(
             id="contaminated",
-            Type="input",
+            Type="input_int",
             valid=[],
             text="how many biscuits are contaminated?",
         )
         yield Question(
+            id="wrong_input",
+            Type="input_string",
+            valid=[
+                "cracked",
+                "burned",
+                "under_cooked",
+                "over_sized",
+                "under_sized",
+                "contaminated",
+            ],
+            text="you mistyped one of the deffects number please choose what you put wrong to modify\n1-cracked\n2-burned\n3-under_cooked\n4-over_sized\n5-under_sized\n6-contaminated",
+        )
+        yield Question(
             id="cracked_size",
-            Type="input",
+            Type="input_float",
             valid=[],
             text="what is the average crack rate(example: 50%, 30%, etc...)?",
         )
         yield Question(
             id="burned_percentage",
-            Type="input",
+            Type="input_float",
             valid=[],
             text="what is the average burn rate(example: 50%, 30%, etc...)?",
         )
         yield Question(
             id="under_cooked_percentage",
-            Type="input",
+            Type="input_float",
             valid=[],
             text="what is the average under_cooked rate(example: 50%, 30%, etc...)?",
         )
         yield Question(
             id="over_sized_size",
-            Type="input",
+            Type="input_float",
             valid=[],
             text="what is the average size(radius) of the biscuits(example: 4cm, 10cm, etc...)?",
         )
@@ -92,38 +105,55 @@ class RecommendationEngine(KnowledgeEngine):
         # "Ask a question and assert the answer""
         self.retract(ask)
         answer = self.ask_user(text, Type, valid)
-        answer = answer.strip().replace("%", "")
         self.declare(Answer(id=id, text=answer))
 
     # Useful functions
     def ask_user(self, question, Type, valid=None):
         # "Ask a question, and return the answer"
         answer = ""
-        while not (self.is_of_type(answer, Type, valid)):
+        while True:
             print(question)
             answer = input()
+
+            ans = self.is_of_type(answer, Type, valid)
+            if ans != None:
+                answer = ans
+                break
+
         return answer
 
     def is_of_type(self, answer, Type, valid):
         # "Check that the answer has the right form"
-        if Type == "input":
-            print(Type)
-            ans = answer.strip().replace("%", "")
-            print("ans", ans)
-            print("num", self.is_a_number(ans))
-            return self.is_a_number(ans)
+        ans = answer.strip().replace("%", "")
+        if Type == "input_string":
+            if ans in valid:
+                return ans
+        elif Type == "input_int":
+            return self.is_a_int(ans)
+        elif Type == "input_float":
+            return self.is_a_float(ans)
         else:
-            pass
+            return None
 
-    def is_a_number(self, answer):
+    def is_a_int(self, answer):
         try:
             answer = int(answer)
             if answer >= 0:
-                return True
+                return answer
             else:
-                return False
+                return None
         except:
-            return False
+            return None
+
+    def is_a_float(self, answer):
+        try:
+            answer = float(answer)
+            if answer >= 0:
+                return answer
+            else:
+                return None
+        except:
+            return None
 
     # Input validation rules
 
@@ -199,8 +229,8 @@ class RecommendationEngine(KnowledgeEngine):
         AS.uns << Answer(id=L("under_sized"), text=MATCH.ans6),
         AS.co << Answer(id=L("contaminated"), text=MATCH.ans7),
         TEST(
-            lambda ans1, ans2, ans3, ans4, ans5, ans6, ans7: int(ans1)
-            < int(ans2) + int(ans3) + int(ans4) + int(ans5) + int(ans6) + int(ans7)
+            lambda ans1, ans2, ans3, ans4, ans5, ans6, ans7: ans1
+            < ans2 + ans3 + ans4 + ans5 + ans6 + ans7
         ),
         salience=50,
     )
@@ -212,6 +242,107 @@ class RecommendationEngine(KnowledgeEngine):
         self.retract(uns)
         self.retract(co)
         self.declare(Fact(ask="cracked"))
+
+    @Rule(
+        NOT(Fact(ask="wrong_input")),
+        Answer(id=L("deffected"), text=MATCH.ans1),
+        Answer(id=L("cracked"), text=MATCH.ans2),
+        Answer(id=L("burned"), text=MATCH.ans3),
+        Answer(id=L("under_cooked"), text=MATCH.ans4),
+        Answer(id=L("over_sized"), text=MATCH.ans5),
+        Answer(id=L("under_sized"), text=MATCH.ans6),
+        Answer(id=L("contaminated"), text=MATCH.ans7),
+        TEST(
+            lambda ans1, ans2, ans3, ans4, ans5, ans6, ans7: ans1
+            > ans2 + ans3 + ans4 + ans5 + ans6 + ans7
+        ),
+        salience=50,
+    )
+    def validation3(self):
+        print("validation3")
+        self.declare(Fact(ask="wrong_input"))
+
+    @Rule(
+        AS.wrong << Answer(id=L("wrong_input"), text=MATCH.id),
+        AS.answer << (Answer(id=MATCH.id)),
+        salience=100,
+    )
+    def ReAsk_about_defect_to_modify(self, wrong, answer, id):
+        print("ReAsk_about_defect_to_modify")
+        self.retract(wrong)
+        self.retract(answer)
+        self.declare(Fact(ask=id))
+
+    # the rules for percent
+    @Rule(
+        (Answer(id=L("contaminated"))),
+        NOT(Answer(id=L("cracked_size"))),
+    )
+    def supply_answer_cracked_size(self):
+        print("supply_answer_cracked_size")
+        self.declare(Fact(ask="cracked_size"))
+
+    @Rule(
+        (Answer(id=L("cracked_size"))),
+        NOT(Answer(id=L("burned_percentage"))),
+    )
+    def supply_answer_burned_percentage(self):
+        self.declare(Fact(ask="burned_percentage"))
+
+    @Rule(
+        (Answer(id=L("burned_percentage"))),
+        NOT(Answer(id=L("under_cooked_percentage"))),
+    )
+    def supply_answer_under_cooked_percentage(self):
+        self.declare(Fact(ask="under_cooked_percentage"))
+
+    @Rule(
+        (Answer(id=L("under_cooked_percentage"))),
+        NOT(Answer(id=L("over_sized_size"))),
+    )
+    def supply_answer_size_percentage(self):
+        self.declare(Fact(ask="over_sized_size"))
+
+    # Declaring the rules that declares deffects
+
+    @Rule(
+        (Answer(id=L("cracked"))),
+        (Answer(id=L("cracked_size"), text=MATCH.cr)),
+    )
+    def declare_cracked(self, cr):
+        self.declare(Cracked(size=cr))
+
+    @Rule(
+        (Answer(id=L("burned"))),
+        (Answer(id=L("burned_percentage"), text=MATCH.br)),
+    )
+    def declare_burned(self, br):
+        self.declare(Burned(percentage=br))
+
+    @Rule(
+        (Answer(id=L("under_cooked"))),
+        (AS.cr << Answer(id=L("under_cooked_percentage"), text=MATCH.uc)),
+    )
+    def declare_under_cooked(self, uc):
+        self.declare(UnderCooked(percentage=uc))
+
+    @Rule(
+        (Answer(id=L("over_sized"))),
+        (AS.cr << Answer(id=L("over_sized_size"), text=MATCH.size)),
+    )
+    def declare_over_sized(self, size):
+        self.declare(OverSized(size=size))
+
+    @Rule(
+        (Answer(id=L("under_sized"))),
+        (AS.cr << Answer(id=L("over_sized_size"), text=MATCH.size)),
+    )
+    def declare_under_sized(self, size):
+        self.declare(UnderSized(size=size))
+
+    @Rule((Answer(id=L("contaminated"))), salience=20)
+    def declare_under_sized(self):
+        self.declare(Contaminated())
 
     # Cracked rules
     @Rule(Cracked(size=P(lambda x: x >= 20)))
@@ -255,25 +386,25 @@ class RecommendationEngine(KnowledgeEngine):
         )
 
     # Size-related rules
-    @Rule(OverSized(size=P(lambda x: x >= 15)))
+    @Rule(OverSized(size=P(lambda x: x > 15)))
     def critical_oversized(self):
         self.declare(
-            Fact(recommendation="Major oversizing (≥15%) - recalibrate equipment")
+            Fact(recommendation="Major oversizing (>15) - recalibrate equipment")
         )
 
-    @Rule(OverSized(size=P(lambda x: 5 <= x < 15)))
+    @Rule(OverSized(size=P(lambda x: 12 <= x <= 15)))
     def moderate_oversized(self):
-        self.declare(Fact(recommendation="Check sizing equipment (5-15% oversized)"))
+        self.declare(Fact(recommendation="Check sizing equipment (12-15 oversized)"))
 
-    @Rule(UnderSized(size=P(lambda x: x >= 15)))
+    @Rule(UnderSized(size=P(lambda x: 10 <= x <= 12)))
+    def moderate_undersized(self):
+        self.declare(Fact(recommendation="Check sizing equipment (10-12 undersized)"))
+
+    @Rule(UnderSized(size=P(lambda x: x < 10)))
     def critical_undersized(self):
         self.declare(
-            Fact(recommendation="Major undersizing (≥15%) - recalibrate equipment")
+            Fact(recommendation="Major undersizing (<10) - recalibrate equipment")
         )
-
-    @Rule(UnderSized(size=P(lambda x: 5 <= x < 15)))
-    def moderate_undersized(self):
-        self.declare(Fact(recommendation="Check sizing equipment (5-15% undersized)"))
 
     # Contamination rule
     @Rule(Contaminated(), salience=1000)
